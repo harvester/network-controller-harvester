ade23898c5a43c59c46bf00c862407032ef2cb20
yaocw2020 <canwu.yao@suse.com>
2022-09-06 17:23:29 +0800

    Add harvester network webhook

---
+++ pkg/webhook/nad/mutator.go
@@ -0,0 +1,66 @@
+package nad
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	cniv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	"github.com/yaocw2020/webhook/pkg/types"
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/harvester/harvester-network-controller/pkg/network/iface"
+	"github.com/harvester/harvester-network-controller/pkg/utils"
+)
+
+type nadMutator struct {
+	types.DefaultMutator
+}
+
+var _ types.Mutator = &nadMutator{}
+
+func NewNadMutator() *nadMutator {
+	return &nadMutator{}
+}
+
+func (n *nadMutator) Create(request *types.Request, newObj runtime.Object) (types.Patch, error) {
+	netAttachDef := newObj.(*cniv1.NetworkAttachmentDefinition)
+
+	labels := netAttachDef.Labels
+	if labels == nil {
+		labels = make(map[string]string)
+	}
+
+	netconf := &utils.NetConf{}
+	if err := json.Unmarshal([]byte(netAttachDef.Spec.Config), netconf); err != nil {
+		return nil, fmt.Errorf(createErr, netAttachDef.Namespace, netAttachDef.Name, err)
+	}
+	labels[utils.KeyVlanLabel] = strconv.Itoa(netconf.Vlan)
+
+	lenOfBrName := len(netconf.BrName)
+	if lenOfBrName < 3 {
+		return nil, fmt.Errorf(createErr, netAttachDef.Namespace, netAttachDef.Name, fmt.Errorf("the length of brName must be more than 3"))
+	}
+	labels[utils.KeyClusterNetworkLabel] = netconf.BrName[:lenOfBrName-len(iface.BridgeSuffix)]
+
+	return types.Patch{
+		types.PatchOp{
+			Op:    types.PatchOpReplace,
+			Path:  "/metadata/labels",
+			Value: labels,
+		}}, nil
+}
+
+func (n *nadMutator) Resource() types.Resource {
+	return types.Resource{
+		Names:      []string{"network-attachment-definitions"},
+		Scope:      admissionregv1.NamespacedScope,
+		APIGroup:   cniv1.SchemeGroupVersion.Group,
+		APIVersion: cniv1.SchemeGroupVersion.Version,
+		ObjectType: &cniv1.NetworkAttachmentDefinition{},
+		OperationTypes: []admissionregv1.OperationType{
+			admissionregv1.Create,
+		},
+	}
+}
