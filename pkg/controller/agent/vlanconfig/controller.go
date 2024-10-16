harvester/network-controller-harvester

commit 652162909d48f81d2759c49538e2ac42ecd75eb9

Disable iptables bridge forwarding on initialization

pkg/network/vlan/vlan.go
@@ -142,3 +142,9 @@ func (v *Vlan) Bridge() *iface.Bridge {
 func (v *Vlan) Uplink() *iface.Link {
 	return v.uplink
 }
+
+func init() {
+	if err := iface.DisableBridgeNF(); err != nil {
+		klog.Fatalf("disable net.bridge.bridge-nf-call-iptables failed, error: %v", err)
+	}
+}

pkg/network/iface/bridge.go
@@ -31,10 +31,6 @@ func NewBridge(name string) *Bridge {
 // Ensure bridge
 // set promiscuous mod default
 func (br *Bridge) Ensure() error {
-	if err := disableBridgeNF(); err != nil {
-		return fmt.Errorf("disable net.bridge.bridge-nf-call-iptables failed, error: %w", err)
-	}
-
 	if err := netlink.LinkAdd(br); err != nil && err != syscall.EEXIST {
 		return fmt.Errorf("add iface failed, error: %w, iface: %v", err, br)
 	}
@@ -67,7 +63,7 @@ func (br *Bridge) Ensure() error {
 	return br.Fetch()
 }
 
-func disableBridgeNF() error {
+func DisableBridgeNF() error {
 	return utils.EnsureSysctlValue(bridgeNFCallIptables, "0")
 }
 

commit 223a006fac297bb56a7347472377ebd87a930d9a

agent: move the iptables bridge forwarding disable to agent register
The DisableBridgeNF() placed in init() would cause phantom error
message while restart/kill the network-manager pod. Move it to
agent register to prevent the unexpected invoke.

Fixes: 652162909d48 ("Disable iptables bridge forwarding on initializatio")

pkg/controller/agent/vlanconfig/controller.go
@@ -62,6 +62,10 @@ func Register(ctx context.Context, management *config.Management) error {
 		cnCache:    cns.Cache(),
 	}
 
+	if err := handler.initialize(); err != nil {
+		return fmt.Errorf("initialize error: %w", err)
+	}
+
 	vcs.OnChange(ctx, ControllerName, handler.OnChange)
 	vcs.OnRemove(ctx, ControllerName, handler.OnRemove)
 
@@ -119,6 +123,13 @@ func (h Handler) OnRemove(_ string, vc *networkv1.VlanConfig) (*networkv1.VlanCo
 	}
 
 	return vc, nil
+}
+
+func (h Handler) initialize() error {
+	if err := iface.DisableBridgeNF(); err != nil {
+		return fmt.Errorf("disable net.bridge.bridge-nf-call-iptables failed, error: %v", err)
+	}
+	return nil
 }
 
 // MatchNode will also return the executed vlanconfig with the same clusterNetwork on this node if existing

pkg/network/vlan/vlan.go
@@ -142,9 +142,3 @@ func (v *Vlan) Bridge() *iface.Bridge {
 func (v *Vlan) Uplink() *iface.Link {
 	return v.uplink
 }
-
-func init() {
-	if err := iface.DisableBridgeNF(); err != nil {
-		klog.Fatalf("disable net.bridge.bridge-nf-call-iptables failed, error: %v", err)
-	}
-}