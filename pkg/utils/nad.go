7214752d7dd5cf72e9b669c2a4636be275590cf0
Jian Wang <jian.wang@suse.com>
2025-02-24 13:03:01 +0100

    Add more processing on network controller
    Enforce the validator webhook
    Add test code for all cases with a couple of fake clients
    Differentiate mgmt network as it has no VlanConfig
    Add processing of StorageNetwork
    Fix some minor bugs
    
    Signed-off-by: Jian Wang <jian.wang@suse.com>
    (cherry picked from commit 97bf188a9f84c2d0108bf797c033c75c47072a26)

--- pkg/utils/nad.go
+++ pkg/utils/nad.go
@@ -4,9 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	"strings"
+
+	ctlcniv1 "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 
 	cniv1 "github.com/containernetworking/cni/pkg/types"
 	nadv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+
+	harvesterutil "github.com/harvester/harvester/pkg/util"
 )
 
 type Connectivity string
@@ -131,4 +139,93 @@ func isMaskZero(ipnet *net.IPNet) bool {
 	}
 
 	return true
+}
+
+// if this nad is a storagenetwork nad
+func IsStorageNetworkNad(nad *nadv1.NetworkAttachmentDefinition) bool {
+	if nad == nil || nad.Namespace != harvesterutil.HarvesterSystemNamespaceName {
+		return false
+	}
+
+	// seems Harvester webhook has no protection on this annotation
+	if nad.Annotations != nil && nad.Annotations[StorageNetworkAnnotation] == "true" {
+		return true
+	}
+
+	// check name
+	if strings.HasPrefix(nad.Name, StorageNetworkNetAttachDefPrefix) {
+		return true
+	}
+
+	return false
+}
+
+// filter the first active storage network nad from a list of nads
+func FilterFirstActiveStorageNetworkNad(nads []*nadv1.NetworkAttachmentDefinition) *nadv1.NetworkAttachmentDefinition {
+	if len(nads) == 0 {
+		return nil
+	}
+	for _, nad := range nads {
+		if IsStorageNetworkNad(nad) && nad.DeletionTimestamp == nil {
+			return nad
+		}
+	}
+	return nil
+}
+
+type NadGetter struct {
+	nadCache ctlcniv1.NetworkAttachmentDefinitionCache
+}
+
+func NewNadGetter(nadCache ctlcniv1.NetworkAttachmentDefinitionCache) *NadGetter {
+	return &NadGetter{nadCache: nadCache}
+}
+
+// list all nads attached to a cluster network
+func (n *NadGetter) ListNadsOnClusterNetwork(cnName string) ([]*nadv1.NetworkAttachmentDefinition, error) {
+	nads, err := n.nadCache.List(corev1.NamespaceAll, labels.Set(map[string]string{
+		KeyClusterNetworkLabel: cnName,
+	}).AsSelector())
+	if err != nil {
+		return nil, err
+	}
+
+	if len(nads) == 0 {
+		return nil, nil
+	}
+	return nads, nil
+}
+
+func (n *NadGetter) GetFirstActiveStorageNetworkNadOnClusterNetwork(cnName string) (*nadv1.NetworkAttachmentDefinition, error) {
+	nads, err := n.nadCache.List(harvesterutil.HarvesterSystemNamespaceName, labels.Set(map[string]string{
+		KeyClusterNetworkLabel: cnName,
+	}).AsSelector())
+	if err != nil {
+		return nil, err
+	}
+
+	if len(nads) == 0 {
+		return nil, nil
+	}
+
+	return FilterFirstActiveStorageNetworkNad(nads), nil
+}
+
+func (n *NadGetter) NadNamesOnClusterNetwork(cnName string) ([]string, error) {
+	nads, err := n.ListNadsOnClusterNetwork(cnName)
+	if err != nil {
+		return nil, err
+	}
+	return generateNadNameList(nads), nil
+}
+
+func generateNadNameList(nads []*nadv1.NetworkAttachmentDefinition) []string {
+	if len(nads) == 0 {
+		return nil
+	}
+	nadStrList := make([]string, len(nads))
+	for i, nad := range nads {
+		nadStrList[i] = nad.Namespace + "/" + nad.Name
+	}
+	return nadStrList
 }
--- pkg/webhook/clusternetwork/validator.go
+++ pkg/webhook/clusternetwork/validator.go
@@ -2,7 +2,10 @@ package clusternetwork
 
 import (
 	"fmt"
+	"strings"
 
+	ctlcniv1 "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io/v1"
+	ctlkubevirtv1 "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
 	"github.com/harvester/webhook/pkg/server/admission"
 	admissionregv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -18,18 +21,25 @@ const (
 	createErr = "can't create cluster network %s because %w"
 	updateErr = "can't update cluster network %s because %w"
 	deleteErr = "can't delete cluster network %s because %w"
+
+	maxClusterNetworkNameLen = iface.MaxDeviceNameLen - len(iface.BridgeSuffix)
 )
 
 type CnValidator struct {
 	admission.DefaultValidator
-	vcCache ctlnetworkv1.VlanConfigCache
+
+	nadCache ctlcniv1.NetworkAttachmentDefinitionCache
+	vmiCache ctlkubevirtv1.VirtualMachineInstanceCache
+	vcCache  ctlnetworkv1.VlanConfigCache
 }
 
 var _ admission.Validator = &CnValidator{}
 
-func NewCnValidator(vcCache ctlnetworkv1.VlanConfigCache) *CnValidator {
+func NewCnValidator(nadCache ctlcniv1.NetworkAttachmentDefinitionCache, vmiCache ctlkubevirtv1.VirtualMachineInstanceCache, vcCache ctlnetworkv1.VlanConfigCache) *CnValidator {
 	validator := &CnValidator{
-		vcCache: vcCache,
+		nadCache: nadCache,
+		vmiCache: vmiCache,
+		vcCache:  vcCache,
 	}
 	return validator
 }
@@ -37,17 +47,12 @@ func NewCnValidator(vcCache ctlnetworkv1.VlanConfigCache) *CnValidator {
 func (c *CnValidator) Create(_ *admission.Request, newObj runtime.Object) error {
 	cn := newObj.(*networkv1.ClusterNetwork)
 
-	maxClusterNetworkNameLen := iface.MaxDeviceNameLen - len(iface.BridgeSuffix)
-
 	if len(cn.Name) > maxClusterNetworkNameLen {
 		return fmt.Errorf(createErr, cn.Name, fmt.Errorf("the length of name is more than %d", maxClusterNetworkNameLen))
 	}
 
-	// this label can only be operated by controller
-	if cn.Labels != nil {
-		if _, ok := cn.Labels[utils.KeyUplinkMTU]; ok {
-			return fmt.Errorf(createErr, cn.Name, fmt.Errorf("label %v can't be added", utils.KeyUplinkMTU))
-		}
+	if err := checkMTUOfNewClusterNetwork(cn); err != nil {
+		return fmt.Errorf(createErr, cn.Name, err)
 	}
 
 	return nil
@@ -57,23 +62,12 @@ func (c *CnValidator) Update(_ *admission.Request, oldObj, newObj runtime.Object
 	oldCn := oldObj.(*networkv1.ClusterNetwork)
 	newCn := newObj.(*networkv1.ClusterNetwork)
 
-	// user can't add or update this label
-	// user can delete the label
-	oldMtu := ""
-	if oldCn.Labels != nil {
-		if mtu, ok := oldCn.Labels[utils.KeyUplinkMTU]; ok {
-			oldMtu = mtu
-		}
+	if err := c.checkMTUOfUpdatedClusterNetwork(oldCn, newCn); err != nil {
+		return fmt.Errorf(updateErr, newCn.Name, err)
 	}
-	if newCn.Labels != nil {
-		if mtu, ok := newCn.Labels[utils.KeyUplinkMTU]; ok {
-			if oldMtu == "" {
-				return fmt.Errorf(updateErr, newCn.Name, fmt.Errorf("label %v can't be added", utils.KeyUplinkMTU))
-			}
-			if mtu != oldMtu {
-				return fmt.Errorf(updateErr, newCn.Name, fmt.Errorf("label %v can't be updated", utils.KeyUplinkMTU))
-			}
-		}
+
+	if err := c.checkMTUOfUpdatedMgmtClusterNetwork(oldCn, newCn); err != nil {
+		return fmt.Errorf(updateErr, newCn.Name, err)
 	}
 
 	return nil
@@ -86,11 +80,12 @@ func (c *CnValidator) Delete(_ *admission.Request, oldObj runtime.Object) error 
 		return fmt.Errorf(deleteErr, cn.Name, fmt.Errorf("it is not allowed"))
 	}
 
+	// all related vcs should be deleted/migrated to others
 	vcs, err := c.vcCache.List(labels.Set{
 		utils.KeyClusterNetworkLabel: cn.Name,
 	}.AsSelector())
 	if err != nil {
-		return err
+		return fmt.Errorf(deleteErr, cn.Name, fmt.Errorf("failed to list vlanconfig error %w", err))
 	}
 
 	if len(vcs) > 0 {
@@ -101,6 +96,18 @@ func (c *CnValidator) Delete(_ *admission.Request, oldObj runtime.Object) error 
 		return fmt.Errorf(deleteErr, cn.Name, fmt.Errorf("vlanconfig(s) %v under this clusternetwork are still existing", vcNameList))
 	}
 
+	// all related nads should be deleted
+	nadGetter := utils.NewNadGetter(c.nadCache)
+	nadStrList, err := nadGetter.NadNamesOnClusterNetwork(cn.Name)
+	if err != nil {
+		return fmt.Errorf(deleteErr, cn.Name, err)
+	}
+
+	if len(nadStrList) > 0 {
+		return fmt.Errorf(deleteErr, cn.Name, fmt.Errorf("nads(s) %v under this clusternetwork are still existing", strings.Join(nadStrList, ", ")))
+	}
+
+	// no need to check vmi, vm, both of them need to be stopped &/ removed related interfaces/networks when deleting nads
 	return nil
 }
 
@@ -117,4 +124,98 @@ func (c *CnValidator) Resource() admission.Resource {
 			admissionregv1.Delete,
 		},
 	}
+}
+
+func checkMTUOfNewClusterNetwork(cn *networkv1.ClusterNetwork) error {
+	if cn == nil || cn.Annotations == nil {
+		return nil
+	}
+
+	// for none-mgmt cluster network, this annotation can only be operated by controller
+	if _, ok := cn.Annotations[utils.KeyUplinkMTU]; ok {
+		return fmt.Errorf("annotation %v can't be added", utils.KeyUplinkMTU)
+	}
+	return nil
+}
+
+// for none-mgmt cluster network
+func (c *CnValidator) checkMTUOfUpdatedClusterNetwork(oldCn, newCn *networkv1.ClusterNetwork) error {
+	if oldCn == nil || newCn == nil || newCn.Name == utils.ManagementClusterNetworkName {
+		return nil
+	}
+
+	newMtu := utils.DefaultMTU
+	var err error
+	if mtu, ok := newCn.Annotations[utils.KeyUplinkMTU]; ok {
+		if newMtu, err = utils.GetMTUFromAnnotation(mtu); err != nil {
+			return err
+		}
+	}
+
+	// ensure clusternetwork's MTU is same with all vlanconfigs
+	vcs, err := c.vcCache.List(labels.Set(map[string]string{
+		utils.KeyClusterNetworkLabel: newCn.Name,
+	}).AsSelector())
+	if err != nil {
+		return err
+	}
+
+	for _, vc := range vcs {
+		if !utils.AreEqualMTUs(vc.Spec.Uplink.LinkAttrs.MTU, newMtu) {
+			return fmt.Errorf("clusternetwork has MTU %v, but the vlanconfigs %v has another MTU %v", newMtu, vc.Name, vc.Spec.Uplink.LinkAttrs.MTU)
+		}
+	}
+
+	return nil
+}
+
+// mgmt cluster network, there is no vlanconfig to configure MTU, the MTU is configured in node installation stage and saved to local file
+// later we need to convert each node's network configuration to a related vlanconfig object
+// currently, if user plans to set a none-default MTU value, then it can be updated via clusternetwork mgmt annotation
+func (c *CnValidator) checkMTUOfUpdatedMgmtClusterNetwork(oldCn, newCn *networkv1.ClusterNetwork) error {
+	if oldCn == nil || newCn == nil || newCn.Name != utils.ManagementClusterNetworkName {
+		return nil
+	}
+
+	// mgmt network, MTU can be updated
+	newMtu := utils.DefaultMTU
+	var err error
+	if mtu, ok := newCn.Annotations[utils.KeyUplinkMTU]; ok {
+		if newMtu, err = utils.GetMTUFromAnnotation(mtu); err != nil {
+			return err
+		}
+	}
+
+	oldMtu := utils.DefaultMTU
+	if mtu, ok := oldCn.Annotations[utils.KeyUplinkMTU]; ok {
+		if oldMtu, err = utils.GetMTUFromAnnotation(mtu); err != nil {
+			return err
+		}
+	}
+
+	// MTU does not change
+	if utils.AreEqualMTUs(oldMtu, newMtu) {
+		return nil
+	}
+
+	// for mgmt network, the nad is not tied to any vlanconfig, check nad directly
+	nadGetter := utils.NewNadGetter(c.nadCache)
+	nads, err := nadGetter.ListNadsOnClusterNetwork(newCn.Name)
+	if err != nil {
+		return err
+	}
+
+	if nad := utils.FilterFirstActiveStorageNetworkNad(nads); nad != nil {
+		return fmt.Errorf("the MTU can't be changed from %v to %v as storage network nad %s is still attached", oldMtu, newMtu, nad.Name)
+	}
+
+	vmiGetter := utils.NewVmiGetter(c.vmiCache)
+	// get all, no filter
+	if vmiStrList, err := vmiGetter.VmiNamesWhoUseNads(nads, false, nil); err != nil {
+		return err
+	} else if len(vmiStrList) > 0 {
+		return fmt.Errorf("the MTU can't be changed from %v to %v as following VMs must be stopped at first: %s", oldMtu, newMtu, strings.Join(vmiStrList, ", "))
+	}
+
+	return nil
 }
