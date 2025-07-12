59c334c9c1d7df5af6cc667be160f214ea6b4ecd
Gaurav Mehta <gaurav.mehta@suse.com>
2025-07-12 22:24:22 +1000

    minor changes to include goimports to tidy up unused imports in generated code introduced by current version of wrangler, and add a new generate target
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    (cherry picked from commit 4ec00f753ff2d38ca4664e4dd8d53418c7ee7266)

--- main.go
+++ main.go
@@ -2,5 +2,7 @@
 //go:generate /bin/rm -rf pkg/generated
 //go:generate go run pkg/codegen/main.go
 //go:generate /bin/bash scripts/generate-manifest
+//go:generate goimports -w ./pkg/apis
+//go:generate goimports -w ./pkg/generated
 
 package main
