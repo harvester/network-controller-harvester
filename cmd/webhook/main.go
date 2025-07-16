5340418609c09fb17df53cfd44344d876b8dd9af
Gaurav Mehta <gaurav.mehta@suse.com>
2025-07-16 13:03:42 +1000

    minor changes to make subnet validation optional
    passed info about crd existance to the nad validator. This can be used to fine tune nad validations where a kubeovn based network may be used without having the crd installed
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    
    minor changes to make subnet validation optional
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"syscall"
+	"time"
 
-	ctlcni "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io"
-	ctlcniv1 "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io/v1"
-	ctlkubevirt "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io"
-	ctlkubevirtv1 "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
 	"github.com/harvester/webhook/pkg/config"
 	"github.com/harvester/webhook/pkg/server"
 	ctlcore "github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
@@ -20,6 +18,16 @@ import (
 	"github.com/urfave/cli"
 	"k8s.io/client-go/rest"
 	kubevirtv1 "kubevirt.io/api/core/v1"
+
+	ctlcni "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io"
+	ctlcniv1 "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io/v1"
+	ctlkubevirt "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io"
+	ctlkubevirtv1 "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
+
+	"github.com/harvester/webhook/pkg/server/admission"
+	apiextensionsClient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	kubeovnnetwork "github.com/harvester/harvester-network-controller/pkg/generated/controllers/kubeovn.io"
 	kubeovnnetworkv1 "github.com/harvester/harvester-network-controller/pkg/generated/controllers/kubeovn.io/v1"
@@ -34,6 +42,8 @@ import (
 
 const (
 	name = "harvester-network-webhook"
+
+	subnetsCRDName = "subnets.kubeovn.io"
 )
 
 var (
@@ -113,7 +123,13 @@ func main() {
 }
 
 func run(ctx context.Context, cfg *rest.Config, options *config.Options) error {
-	c, err := newCaches(ctx, cfg, options.Threadiness)
+	// check if subnet crd exists
+	crdExists, err := isSubnetsCRDPresent(ctx, cfg)
+	if err != nil {
+		return err
+	}
+
+	c, err := newCaches(ctx, cfg, options.Threadiness, crdExists)
 	if err != nil {
 		return err
 	}
@@ -127,18 +143,27 @@ func run(ctx context.Context, cfg *rest.Config, options *config.Options) error {
 		return fmt.Errorf("failed to register mutators: %v", err)
 	}
 
-	if err := webhookServer.RegisterValidators(
+	validators := []admission.Validator{
 		clusternetwork.NewCnValidator(c.nadCache, c.vmiCache, c.vcCache),
-		nad.NewNadValidator(c.vmCache, c.vmiCache, c.cnCache, c.vcCache, c.kubeovnsubnetCache),
+		nad.NewNadValidator(c.vmCache, c.vmiCache, c.cnCache, c.vcCache, c.kubeovnsubnetCache, crdExists),
 		vlanconfig.NewVlanConfigValidator(c.nadCache, c.vcCache, c.vsCache, c.vmiCache, c.cnCache),
-		subnet.NewSubnetValidator(c.nadCache, c.kubeovnsubnetCache, c.kubeovnvpcCache),
-	); err != nil {
+	}
+
+	if crdExists {
+		validators = append(validators, subnet.NewSubnetValidator(c.nadCache, c.kubeovnsubnetCache, c.kubeovnvpcCache))
+	}
+
+	if err := webhookServer.RegisterValidators(validators...); err != nil {
 		return fmt.Errorf("failed to register validators: %v", err)
 	}
 
 	if err := webhookServer.Start(); err != nil {
 		return err
 	}
+
+	go func() {
+		watchAndTriggerReload(ctx, crdExists, cfg)
+	}()
 
 	<-ctx.Done()
 
@@ -157,7 +182,7 @@ type caches struct {
 	kubeovnvpcCache    kubeovnnetworkv1.VpcCache
 }
 
-func newCaches(ctx context.Context, cfg *rest.Config, threadiness int) (*caches, error) {
+func newCaches(ctx context.Context, cfg *rest.Config, threadiness int, crdExists bool) (*caches, error) {
 	var starters []start.Starter
 
 	kubevirtFactory := ctlkubevirt.NewFactoryFromConfigOrDie(cfg)
@@ -168,20 +193,23 @@ func newCaches(ctx context.Context, cfg *rest.Config, threadiness int) (*caches,
 	starters = append(starters, harvesterNetworkFactory)
 	coreFactory := ctlcore.NewFactoryFromConfigOrDie(cfg)
 	starters = append(starters, coreFactory)
-	kubeovnFactory := kubeovnnetwork.NewFactoryFromConfigOrDie(cfg)
-	starters = append(starters, kubeovnFactory)
 
 	// must declare cache before starting informers
 	c := &caches{
-		nadCache:           cniFactory.K8s().V1().NetworkAttachmentDefinition().Cache(),
-		vmCache:            kubevirtFactory.Kubevirt().V1().VirtualMachine().Cache(),
-		vmiCache:           kubevirtFactory.Kubevirt().V1().VirtualMachineInstance().Cache(),
-		vcCache:            harvesterNetworkFactory.Network().V1beta1().VlanConfig().Cache(),
-		vsCache:            harvesterNetworkFactory.Network().V1beta1().VlanStatus().Cache(),
-		cnCache:            harvesterNetworkFactory.Network().V1beta1().ClusterNetwork().Cache(),
-		nodeCache:          coreFactory.Core().V1().Node().Cache(),
-		kubeovnsubnetCache: kubeovnFactory.Kubeovn().V1().Subnet().Cache(),
-		kubeovnvpcCache:    kubeovnFactory.Kubeovn().V1().Vpc().Cache(),
+		nadCache:  cniFactory.K8s().V1().NetworkAttachmentDefinition().Cache(),
+		vmCache:   kubevirtFactory.Kubevirt().V1().VirtualMachine().Cache(),
+		vmiCache:  kubevirtFactory.Kubevirt().V1().VirtualMachineInstance().Cache(),
+		vcCache:   harvesterNetworkFactory.Network().V1beta1().VlanConfig().Cache(),
+		vsCache:   harvesterNetworkFactory.Network().V1beta1().VlanStatus().Cache(),
+		cnCache:   harvesterNetworkFactory.Network().V1beta1().ClusterNetwork().Cache(),
+		nodeCache: coreFactory.Core().V1().Node().Cache(),
+	}
+
+	if crdExists {
+		kubeovnFactory := kubeovnnetwork.NewFactoryFromConfigOrDie(cfg)
+		starters = append(starters, kubeovnFactory)
+		c.kubeovnsubnetCache = kubeovnFactory.Kubeovn().V1().Subnet().Cache()
+		c.kubeovnvpcCache = kubeovnFactory.Kubeovn().V1().Vpc().Cache()
 	}
 	// Indexer must be added before starting the informer, otherwise panic `cannot add indexers to running index` happens
 	c.vmiCache.AddIndexer(utils.VMByNetworkIndex, vmiByNetwork)
@@ -225,4 +253,45 @@ func vmByNetwork(obj *kubevirtv1.VirtualMachine) ([]string, error) {
 		networkNameList = append(networkNameList, network.NetworkSource.Multus.NetworkName)
 	}
 	return networkNameList, nil
+}
+
+// isSubnetsCRDPresent checks if the subnets crd is installed
+func isSubnetsCRDPresent(ctx context.Context, cfg *rest.Config) (bool, error) {
+	client, err := apiextensionsClient.NewForConfig(cfg)
+	if err != nil {
+		return false, fmt.Errorf("error initialising api extensions client")
+	}
+	obj, err := client.ApiextensionsV1().CustomResourceDefinitions().Get(ctx, subnetsCRDName, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("error looking up crd %s: %w", subnetsCRDName, err)
+	}
+
+	if !obj.DeletionTimestamp.IsZero() {
+		logrus.Infof("%s crd has a deletion time stamp set, failing check", subnetsCRDName)
+		return false, nil
+	}
+	return true, nil
+}
+
+// watchAndTriggerReload will mark the context done if there is a change in the crd status
+func watchAndTriggerReload(ctx context.Context, initalBootStatus bool, cfg *rest.Config) {
+	for {
+		checkStatus, err := isSubnetsCRDPresent(ctx, cfg)
+		if err == nil {
+			if initalBootStatus == checkStatus {
+				logrus.Debugf("no change in subnet crd status, sleeping")
+				time.Sleep(30 * time.Second)
+				continue
+			}
+			break
+		}
+		// sleep in case an error is returned
+		time.Sleep(30 * time.Second)
+	}
+	logrus.Info("change in crd states, triggering shutdown of webhook server")
+	// send interrupt to self to trigger shutdown and restart
+	syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 }
